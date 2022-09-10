d3344c797c7e020b0fbdfea67c3b031982fbf2ee
Danish Ghaffar <90974992+mdanishghaffar@users.noreply.github.com>
2022-09-10 13:13:44 +0500

    University Admission Management System, Project Upload!

---
+++ UMS/uams/uams/BL/Student.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace uams.BL
+{
+    class Student
+    {
+
+        public string name;
+        public int age;
+        public double fscMarks;
+        public double ecatMarks;
+        public double merit;
+        public List<DegreeProgram> preferences;
+        public List<Subject> regSubject;
+        public DegreeProgram regDegree;
+        
+        public Student(string name, int age, double fscMarks, double ecatMarks, List<DegreeProgram> preferences)
+        {
+            this.name = name;
+            this.age = age;
+            this.fscMarks = fscMarks;
+            this.ecatMarks = ecatMarks;
+            this.preferences = preferences;
+            regSubject = new List<Subject>();
+            
+        }
+
+        public void calculateMerit()
+        {
+            this.merit = (((fscMarks / 1100) * 0.45F) + ((ecatMarks / 400) * 0.55F)) * 100;
+         
+        }
+
+        
+        public bool regStudentSubject(Subject s)
+        {
+            int stCH = getCreditHours();
+            if (regDegree != null && regDegree.isSubjectExists(s) && stCH + s.getCreditHours() <= 9)
+            {
+                regSubject.Add(s);
+                return true;
+            }
+            else
+            {
+                return false;
+                
+            }
+        }
+
+        public int getCreditHours()
+        {
+            int count = 0;
+            foreach (Subject sub in regSubject)
+            {
+                count = count + sub.getCreditHours();
+            }
+            return count;
+        }
+
+        public float calculateFee()
+        {
+            float fee = 0;
+            if (regDegree != null)
+            {
+                foreach (Subject sub in regSubject)
+                {
+                    fee = fee + sub.getSubjectFees();
+                }
+            }
+            return fee;
+        }
+
+        
+    }
+}
---
+++ UMS/uams/uams/UI/StudentUI.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using uams.BL;
+using uams.DL;
+
+namespace uams.UI
+{
+    class StudentUI
+    {
+        public static void printStudents()
+        {
+            foreach (Student s in StudentDL.studentList)
+            {
+                if (s.regDegree != null)
+                {
+                    Console.WriteLine(s.name + " got Admission in " + s.regDegree.getDegreeName());
+                }
+                else
+                {
+                    Console.WriteLine(s.name + " did not get Admission");
+
+                }
+            }
+        }
+
+        
+
+        public static void viewStudentInDegree(string degName)
+        {
+            Console.WriteLine("Name\tFSC\tEcat\tAge");
+            foreach (Student s in StudentDL.studentList)
+            {
+                if (s.regDegree != null)
+                {
+                    if (degName == s.regDegree.getDegreeName())
+                    {
+                        Console.WriteLine(s.name + "\t" + s.fscMarks + "\t" + s.ecatMarks + "\t" + s.age);
+                    }
+                }
+            }
+        }
+
+        public static void viewRegisteredStudents()
+        {
+            Console.WriteLine("Name\tFSC\tEcat\tAge");
+            foreach (Student s in StudentDL.studentList)
+            {
+                if (s.regDegree != null)
+                {
+                    Console.WriteLine(s.name + "\t" + s.fscMarks + "\t" + s.ecatMarks + "\t" + s.age);
+                }
+            }
+        }
+
+        public static Student takeInputForStudent()
+        {
+            string name;
+            int age;
+            double fscMarks;
+            double ecatMarks;
+            List<DegreeProgram> preferences = new List<DegreeProgram>();
+            Console.Write("Enter Student Name: ");
+            name = Console.ReadLine();
+            Console.Write("Enter Student Age: ");
+            age = int.Parse(Console.ReadLine());
+            Console.Write("Enter Student FSc Marks: ");
+            fscMarks = double.Parse(Console.ReadLine());
+            Console.Write("Enter Student Ecat Marks: ");
+            ecatMarks = double.Parse(Console.ReadLine());
+            Console.WriteLine("Available Degree Programs");
+            DegreeProgramUI.viewDegreePrograms();
+
+            Console.Write("Enter how many preferences to Enter: ");
+            int Count = int.Parse(Console.ReadLine());
+            for (int x = 0; x < Count; x++)
+            {
+                string degName = Console.ReadLine();
+                bool flag = false;
+                foreach (DegreeProgram dp in DegreeProgramDL.programList)
+                {
+                    if (degName == dp.getDegreeName() && !(preferences.Contains(dp)))
+                    {
+                        preferences.Add(dp);
+                        flag = true;
+                    }
+
+                }
+                if (flag == false)
+                {
+                    Console.WriteLine("Enter Valid Degree Program Name");
+                    x--;
+                }
+            }
+            Student s = new Student(name, age, fscMarks, ecatMarks, preferences);
+            return s;
+
+        }
+
+        public static void calculateFeeForAll()
+        {
+            foreach (Student s in StudentDL.studentList)
+            {
+                if (s.regDegree != null)
+                {
+                    Console.WriteLine(s.name + " has " + s.calculateFee() + " fees");
+                }
+            }
+        }
+    }
+}
